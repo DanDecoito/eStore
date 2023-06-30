6170bdad77e2041bcb4cb84f6dfe53629ed2ecd5
DanDecoito <116022076+DanDecoito@users.noreply.github.com>
2023-06-29 19:35:12 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eStore';
+
+  isMenuOpen: boolean = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+}
 }
